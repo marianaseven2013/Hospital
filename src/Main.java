b3f81afb2e45d422f813953c27abc1d3483bb442
marianaseven2013 <gmluch@scl.edu.gt>
2024-07-29 08:02:16 -0600

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                        //Doctor
+                        //Enfermeras
+                        //Pacientes
+                        //Cirujanos
+                        //Secretarías
+                        //Fisioterapeutas
+                        //Farmacéuticos
+                        //Paramedicos
+                        //Pediatras
+                        //Camilleros
+
+                        //Sala de espera
+                        //Morgue
+                        //Sala Quírofano
+
+    }
+
+}
